4fe99ea2452dd2fc64c4c312b1252ba3670433d0
hbcheon <hbcheon@iteyes.co.kr>
2022-01-03 15:12:26 +0900

    개발현황 서버 데이터 요청 - Restfull(ToastUI) 연결

--- src/api/http.js
+++ src/api/http.js
@@ -2,13 +2,19 @@ import axios from 'axios';
 import { setInterceptors } from './interceptors';
 
 function createAxiosService() {
-
-  const axiosService = axios.create({
-    withCredentials: true,
-    baseURL: process.env.VUE_APP_API + "/api/",
+  return axios.create({
+    baseURL: process.env.VUE_APP_API,
+    timeout:50000
   });
-    
+}
+
+function createAxiosServiceWithAuth(url) {
+  const axiosService = axios.create({
+    baseURL: `${process.env.VUE_APP_API}/${url}`,
+  });
+
   return setInterceptors(axiosService);
 }
 
 export const axiosService = createAxiosService();
+export const posts = createAxiosServiceWithAuth('posts');
