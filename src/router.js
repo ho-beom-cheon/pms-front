b056ec5ec72cb2f37b9ed331a8225693bac1bf26
yeon_judy <yjkim@iteyes.co.kr>
2022-04-11 14:17:52 +0900

    #67 [PJTE9110] KMS게시판 조회 신규_first commit

--- src/router.js
+++ src/router.js
@@ -38,6 +38,7 @@ const PJTE9003 = () => import(/* webpackChunkName: "PJTE9003" */ './views/pms/PJ
 const PJTE9005 = () => import(/* webpackChunkName: "PJTE9005" */ './views/pms/PJTE9005.vue'); menuList.push(PJTE9005); menuListStr.push("PJTE9005");
 const PJTE9900 = () => import(/* webpackChunkName: "PJTE9900" */ './views/pms/PJTE9900.vue'); menuList.push(PJTE9900); menuListStr.push("PJTE9900");
 const PJTE9120 = () => import(/* webpackChunkName: "PJTE9120" */ './views/pms/PJTE9120.vue'); menuList.push(PJTE9120); menuListStr.push("PJTE9120");
+const PJTE9110 = () => import(/* webpackChunkName: "PJTE9110" */ './views/pms/PJTE9110.vue'); menuList.push(PJTE9110); menuListStr.push("PJTE9110");
 
 // Vue 와 Router 연결
 Vue.use(Router);
