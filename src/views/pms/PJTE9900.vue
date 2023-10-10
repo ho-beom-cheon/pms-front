8341d45ccaaa1d2c1d1e10a487031b3993672622
kim <shkim23@iteyes.co.k>
2023-10-10 14:38:00 +0900

    test

--- src/views/pms/PJTE9900.vue
+++ src/views/pms/PJTE9900.vue
@@ -16,7 +16,6 @@
               :comboArray="this.comboList"
               @dept_cd_change="dept_cd_change"
               @bkup_id_change="bkup_id_change"
-              @skill_grd_change_iss="skill_grd_change_iss"
               ref="combo1"
           ></combo>
           <li class="filter-item">
@@ -328,7 +327,7 @@ export default {
   methods: {
     dept_cd_change(params) {this.info.dept_cd_selected = params},
     bkup_id_change(params) {this.info.bkup_id_selected = params},
-    skill_grd_change_iss(params) {this.info.skill_grd_selected_iss = params},
+
 
 
     init() {
@@ -845,7 +844,6 @@ export default {
         bkup_id_selected  : '0000000000',                                 //백업ID
         prjt_nm_selected: sessionStorage.getItem("LOGIN_PROJ_ID"),   // 프로젝트ID
         dept_cd_selected  : (sessionStorage.getItem("LOGIN_DEPT_CD").substring(0, 3)) === '100' ? 'TTT':(sessionStorage.getItem("LOGIN_DEPT_CD").substring(0, 3)).concat('00000'), //부문코드
-        skill_grd_selected_iss : "전체",
         week_yymm         : this.week_yymm,                               //기준년월
         over_due_dt_yn    : this.over_due_dt_yn,                          // 미진항목(완료요청일을 넘김) 조회 체크박스
         reg_dt : '',                                                      // 등록일
